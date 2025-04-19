ec54b97b0ef23daf53799d0be393d5a12f2cdcb3
proux <proux03@outlook.com>
2025-04-19 16:33:57 +0300

    убран unused import

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -2,10 +2,8 @@ package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-@ActiveProfiles("h2")
 abstract class BaseTests {
 }
